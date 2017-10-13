2adab374a5e94e887a46e7e069cd4528bddca1ac
MrMokka <sartaren@hotmail.com>
2017-10-13 10:49:58 +0200

    Add files via upload

---
+++ Login/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html><html>
+	<head>
+		<title>Clicker Game Login</title>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	</head>
+	<body>
+
+
+
+		<div id="bodyDiv">
+
+			<div id="formDiv">
+				<form id="nameForm" method="POST" action="MyClickerGame/index.php" autocomplete="off">
+				
+					<input id="inputField" type="text" name="name" size="20" maxlength="18" placeholder="Enter name..." autofocus required>
+
+					<button id="playButton" name="playGame" type="submit">Play Game</button>
+
+				</form>
+			</div>
+
+			<div id="top5_div">
+				
+				<table>
+					
+					<tr>
+						<th>Name</th>
+						<th>Clicks</th>
+						<th>Total Clicks</th>
+					</tr>
+					<tr>
+						<td>MrMokka</td>
+						<td>over 9000</td>
+						<td>Way too much..</td>
+					</tr>
+					<tr>
+						<td>Other ppl</td>
+						<td>200</td>
+						<td>1000</td>
+					</tr>
+					<tr>
+						<td>Other ppl</td>
+						<td>200</td>
+						<td>1000</td>
+					</tr>
+					<tr>
+						<td>Other ppl</td>
+						<td>200</td>
+						<td>1000</td>
+					</tr>
+					<tr>
+						<td>Other ppl</td>
+						<td>200</td>
+						<td>1000</td>
+					</tr>
+
+				</table>
+
+			</div>
+
+		</div>
+
+	</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
